b3b819a699f9864245bf5f6c901a3eb5e6a1d39c
CarnisJr <mirandab950@gmail.com>
2024-11-29 22:20:47 -0500

    Desarrollo de log in

--- src/GUI/ReservaCanchas.java
+++ src/GUI/ReservaCanchas.java
@@ -1,4 +1,49 @@
 package GUI;
 
+import Logic.Usuario;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class ReservaCanchas {
+    private JPanel Principal;
+    private JPasswordField textContrasenia;
+    private JTextField textUsuario;
+    private JButton ingresarButton;
+    private Usuario usuario = new Usuario();
+
+    public ReservaCanchas() {
+        ingresarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                switch (usuario.logIn(textUsuario.getText(), textContrasenia.getText())) {
+
+                    case -1:
+
+                        JOptionPane.showMessageDialog(null, "Datos incorrectos");
+                    break;
+
+                    case 0:
+
+                        JOptionPane.showMessageDialog(null, "Estudiante Logueado Correctamente");
+                    break;
+
+                    case 1:
+
+                        JOptionPane.showMessageDialog(null, "Administrador Logueado Correctamente");
+                    break;
+                }
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("ReservaCanchas");
+        frame.setContentPane(new ReservaCanchas().Principal);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
--- src/Logic/Usuario.java
+++ src/Logic/Usuario.java
@@ -1,4 +1,44 @@
 package Logic;
 
 public class Usuario {
+
+    private String[] usrsAdmins = {"Pepe", "Carlos", "Antonio", "Jorge"};
+    private String[] passUsrsAdmins = {"Pepe1", "Carlos2", "Antonio3", "Jorge4"};
+
+    private String[] usrsEstu = {"Kevin", "Leonardo", "Enner", "Pedro"};
+    private String[] passUsrsEstu = {"Kevin1", "Leonardo2", "Enner3", "Pedro4"};
+
+    private String nombre;
+    private String contrasenia;
+
+    public Usuario() {
+
+        this.nombre = "";
+        this.contrasenia = "";
+    }
+
+    public int logIn(String usuario, String contrasenia) {
+
+        int flag = -1;
+
+        for (int i = 0; i < usrsAdmins.length; i++) {
+
+            if(usuario.equals(usrsAdmins[i]) && contrasenia.equals(passUsrsAdmins[i])) {
+
+                flag = 1;
+                break;
+            }
+        }
+
+        for (int i = 0; i < usrsEstu.length; i++) {
+
+            if(usuario.equals(usrsEstu[i]) && contrasenia.equals(passUsrsEstu[i])) {
+
+                flag = 0;
+                break;
+            }
+        }
+
+        return flag;
+    }
 }
